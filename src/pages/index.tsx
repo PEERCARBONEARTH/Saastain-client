e2705ffd05d33e1456b0224397a0f30c7b8f0a53
VinGitonga <gitongavincent64@gmail.com>
2024-02-21 08:59:56 +0300

    Refactor AuthLayout and remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import { AppEnumRoutes } from "@/types/AppEnumRoutes";
 import { Button } from "@nextui-org/react";
 import { useRouter } from "next/router";
-import Login from "../pages/auth/login";
 
 const index = () => {
 	const router = useRouter();
 	return (
 		<div className="flex items-center h-screen justify-center">
-			<Login />
-			{/* <Button size="lg" onClick={() => router.push(AppEnumRoutes.APP_DASHBOARD)} color="primary">
+			<Button size="lg" onClick={() => router.push(AppEnumRoutes.APP_DASHBOARD)} color="primary">
 				Go to Dashboard
-			</Button> */}
+			</Button>
 		</div>
 	);
 };
