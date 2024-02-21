54ffad0346bf34f98f00d38780fce9a9c46156b2
VinGitonga <gitongavincent64@gmail.com>
2024-02-21 08:50:49 +0300

    Add react-day-picker and update styles in _app.tsx and globals.css

--- src/pages/accounting/add-data/fugitive-emission.tsx
+++ src/pages/accounting/add-data/fugitive-emission.tsx
@@ -4,11 +4,9 @@ import { NextPageWithLayout } from "@/types/Layout";
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import AppInput from "@/components/forms/AppInput";
-import { Card, Spacer, Button, Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
-import { Check, X } from "lucide-react";
+import { Card, Spacer, Button, Breadcrumbs, BreadcrumbItem, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+import { Check, XIcon } from "lucide-react";
 import AppSelect from "@/components/forms/AppSelect";
-
-// import {Button} from "@/components/ui/button";
 
 const schema = z.object({
 	date: z.date(),
@@ -42,71 +40,44 @@ const FugitiveEmission: NextPageWithLayout = () => {
 		{ value: "option3", label: "Option  3" },
 	];
 	return (
-		<Card className="mt-150 p-6 bg-[#E4FCE6] h-full">
-			<Breadcrumbs>
-				<BreadcrumbItem>Accounting</BreadcrumbItem>
-				<BreadcrumbItem>Add Data</BreadcrumbItem>
-			</Breadcrumbs>
-			<div className="p-4 mt-4">
-				<h1 className="text-xl font-bold">Fugitive Emission</h1>
-
-				<p className="mt-6">Here you can input data regarding fuel consumption from vehicles under your ownership / control. Whether it's</p>
-				<p> cars, trucks or airplanes.</p>
-			</div>
-			<Spacer y={6} />
-			<div className="p-4">
-				<FormProvider {...formMethods}>
-					<form onSubmit={formMethods.handleSubmit(onSubmit)}>
-						<div className="mb-6">
+		<Card className="p-6 bg-[#E4FCE6]">
+			<CardHeader>
+				<Breadcrumbs>
+					<BreadcrumbItem>Accounting</BreadcrumbItem>
+					<BreadcrumbItem>Add Data</BreadcrumbItem>
+				</Breadcrumbs>
+			</CardHeader>
+			<FormProvider {...formMethods}>
+				<form onSubmit={formMethods.handleSubmit(onSubmit)}>
+					<CardBody>
+						<div className="space-y-3">
+							<h1 className="text-xl font-bold">Fugitive Emission</h1>
+							<p className="text-gray-600 text-sm">
+								Here you can input data regarding fuel consumption from vehicles under your ownership / control. Whether it's <br /> cars, trucks or airplanes.
+							</p>
+						</div>
+						<div className="my-5">
 							<AppInput label="Select Accounting Period" name="date" type="date" control={formMethods.control} className="w-full" />
-						</div>
-						<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-							<div className="mb-4">
-								<div className="flex items-center gap-4">
-									<AppSelect name="emissionSource" label="Emission Source" control={formMethods.control} options={options} placeholder="Select Source" />
-								</div>
-							</div>
-
-							<div className="mb-4">
-								<div className="flex items-center gap-4 ">
-									<AppSelect label="Fugitive Emission Name" name="emissionName" placeholder="Emission Name" options={options} control={formMethods.control} />
-								</div>
-							</div>
-
-							<div className="mb-4">
-								<div className="flex items-center gap-4 ">
-									<AppSelect label="Emission Gas" name="emissionGas" placeholder="Select Gas" options={options} control={formMethods.control} />
-								</div>
-							</div>
-
-							<div className="mb-4">
-								<div className="flex items-center gap-4 ">
-									<AppSelect label="Unit of Emission" name="unit" options={options} placeholder="Select Unit" control={formMethods.control} />
-								</div>
-							</div>
-
-							<div className="mb-4 gap-8">
-								<div className="flex items-center gap-4 ">
-									<AppInput label="Amount of Leakage Gas" name="gasEmitted" placeholder="0" type="number" control={formMethods.control} />
-								</div>
+							<Spacer y={6} />
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+								<AppSelect name="emissionSource" label="Emission Source" control={formMethods.control} options={options} placeholder="Select Source" />
+								<AppSelect label="Fugitive Emission Name" name="emissionName" placeholder="Emission Name" options={options} control={formMethods.control} />
+								<AppSelect label="Emission Gas" name="emissionGas" placeholder="Select Gas" options={options} control={formMethods.control} />
+								<AppSelect label="Unit of Emission" name="unit" options={options} placeholder="Select Unit" control={formMethods.control} />
+								<AppInput label="Amount of Leakage Gas" name="gasEmitted" placeholder="0" type="number" control={formMethods.control} />
 							</div>
 						</div>
-					</form>
-				</FormProvider>
-
-				<div className="mb-4 gap-8 pr-10">
-					<div className="card-actions  flex flex-col md:flex-row w-full justify-between md:justify-end gap-8">
-						<Button className="btn btn-primary bg-[#5E896E]">
-							<Check size={15} color="white" />
+					</CardBody>
+					<CardFooter className="justify-between md:justify-end gap-5">
+						<Button color="primary" startContent={<Check size={15} />}>
 							Calculate
 						</Button>
-						<Button className="btn btn-secondary border border-[#5E896E]">
-							<X size={15} color="green" />
+						<Button color="primary" startContent={<XIcon size={15} />} variant="bordered">
 							Cancel
 						</Button>
-					</div>
-				</div>
-			</div>
+					</CardFooter>
+				</form>
+			</FormProvider>
 		</Card>
 	);
 };
