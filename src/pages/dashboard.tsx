3b13dad32c0fe860cb1387644ef495e6b96558b1
collinskrotich <kolinsr@gmail.com>
2024-02-27 14:35:19 +0300

    Fixed add buton color on /dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,7 +4,6 @@ import SelectYear from "@/components/select-year-dashboard";
 import AppLayout from "@/layouts/AppLayout";
 import { NextPageWithLayout } from "@/types/Layout";
 import {BreadcrumbItem, Breadcrumbs, Button} from "@nextui-org/react";
-import Image from "next/image";
 import { FaRegFileLines } from "react-icons/fa6";
 import { MdAdd } from "react-icons/md";
 import Footer from "./footer";
@@ -26,7 +25,7 @@ const AppDashboard: NextPageWithLayout = () => {
 				<h3 className="text-4xl">Carbon Footprints record in <span className="text-primary-600">FY2023</span> for <span className="text-primary-600">All Branches</span></h3>
 			</div>
 			<div className="flex space-x-2">
-				<Button color="primary" startContent={<MdAdd className="w-4 h-4" />} size="sm">
+				<Button startContent={<MdAdd className="w-4 h-4" />} size="sm" className="border border-primary text-primary bg-color-grey">
 				Add 
 				</Button>
 				<Button color="primary" startContent={<FaRegFileLines className="w-4 h-4" />} size="sm">
