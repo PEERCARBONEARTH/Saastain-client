06559b7d07e6bc54e64b8665ac57ac95907c8eb5
VinGitonga <gitongavincent64@gmail.com>
2024-06-02 09:34:31 +0300

    Chore: Remove legacy pages, fix serwist bugs

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
-/** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
 
 const nextConfig = {
 	swcMinify: true,
 	output: "standalone",
 };
 
-module.exports = async () => {
-	const withSerwist = (await import("@serwist/next")).default({
-		cacheOnNavigation: true,
-		disable: process.env.NODE_ENV === "development",
-		swSrc: "src/sw.ts",
-		swDest: "public/sw.js",
-		register: true,
-	});
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+	if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+		const withSerwist = (await import("@serwist/next")).default({
+			cacheOnNavigation: true,
+			disable: process.env.NODE_ENV === "development",
+			swSrc: "src/sw.ts",
+			swDest: "public/sw.js",
+			register: true,
+		});
+		return withSerwist(nextConfig);
+	}
 
-	return withSerwist(nextConfig);
+	return nextConfig;
 };
