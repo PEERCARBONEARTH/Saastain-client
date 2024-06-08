c0aa6e1c16d2277097495850cf82f659fc1ec3c8
VinGitonga <gitongavincent64@gmail.com>
2024-06-08 11:20:35 +0300

    Fix bug on create new branch

--- src/hooks/useBranchUtils.ts
+++ src/hooks/useBranchUtils.ts
@@ -6,8 +6,8 @@ import { IBranch } from "@/types/Company";
 const useBranchUtils = () => {
 	const { post } = useApi();
 
-	const addBranchToCompany = useCallback(async (name: string, type: string, address: string) => {
-		const resp = await post<IApiResponse<IBranch>>({ endpoint: IApiEndpoint.CREATE_NEW_COMPANY_BRANCH, data: { name, type, address } });
+	const addBranchToCompany = useCallback(async (name: string, type: string, address: string, companyId: string) => {
+		const resp = await post<IApiResponse<IBranch>>({ endpoint: IApiEndpoint.CREATE_NEW_COMPANY_BRANCH, data: { name, type, address, companyId } });
 
 		return resp.data;
 	}, []);
