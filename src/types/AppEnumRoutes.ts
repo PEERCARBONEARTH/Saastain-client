2f1a763ef5ddb818608e694bc936f9712858ae29
VinGitonga <gitongavincent64@gmail.com>
2024-07-06 23:58:55 +0300

    Update routes for dashboard, companies, docs, loan requests, notifications, settings, users in AppEnumRoutes.ts

--- src/types/AppEnumRoutes.ts
+++ src/types/AppEnumRoutes.ts
@@ -4,27 +4,26 @@ export const enum AppEnumRoutes {
 	AUTH_RESET_PASSWORD = "/auth/reset-password",
 	AUTH_LOGOUT = "/auth/logout",
 
-	APP_DASHBOARD = "/app/dashboard",
+	APP_DASHBOARD = "/dashboard",
 
-	APP_COMPANIES = "/app/companies",
-	APP_COMPANIES_NEW = "/app/companies/new",
-	APP_COMPANY_PROFILE = "/app/companies/profile",
+	APP_COMPANIES = "/companies",
+	APP_COMPANIES_NEW = "/companies/new",
+	APP_COMPANY_PROFILE = "/companies/profile",
 
-	APP_DOCS = "/app/docs",
+	APP_DOCS = "/docs",
 
-	APP_LOAN_REQUESTS = "/app/loan-requests",
-	APP_LOAN_REPAYMENTS = "/app/loan-repayments",
+	APP_LOAN_REQUESTS = "/loan-requests",
+	APP_LOAN_REPAYMENTS = "/loan-repayments",
 
-	APP_NOTIFICATIONS = "/app/notifications",
+	APP_NOTIFICATIONS = "/notifications",
 
-	APP_SETTINGS = "/app/settings",
-	APP_PROFILE = "/app/profile",
-	APP_SUBSCRIPTIONS = "/app/subscriptions",
+	APP_SETTINGS = "/settings",
+	APP_PROFILE = "/profile",
+	APP_SUBSCRIPTIONS = "/subscriptions",
 
-	APP_USERS = "/app/users",
-	APP_USERS_NEW = "/app/users/new",
-	APP_ASSESORS = "/app/assessors",
-	APP_LENDERS = "/app/lenders",
-	APP_USER_PROFILE = "/app/users/profile",
-	APP_USER_WAITLIST = "/app/users/waitlist",
+	APP_USERS = "/users",
+	APP_USERS_NEW = "/users/new",
+	APP_LENDERS = "/users/lenders",
+	APP_USER_PROFILE = "/users/profile",
+	APP_USER_WAITLIST = "/users/waitlist",
 }
