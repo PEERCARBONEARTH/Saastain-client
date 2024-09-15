2e344fa49d2454a22a3b315d1b3f7f1c8c902591
VinGitonga <gitongavincent64@gmail.com>
2024-09-15 11:12:09 +0300

    added config to hide sidebar items

--- src/components/layout/appNavbarMenuItems.tsx
+++ src/components/layout/appNavbarMenuItems.tsx
@@ -37,7 +37,7 @@ export const appNavbarMenuItems = [
 		render={() => (
 			<>
 				<AppNavLinkItem title="Net Zero" icon={<HiOutlineGlobeAlt className="text-primary" size={18} />} href="action-plan/net-zero" />
-				<AppNavLinkItem title="My Projects" icon={<HiBriefcase className="text-primary" size={18} />} href="projects" />
+				<AppNavLinkItem show={false} title="My Projects" icon={<HiBriefcase className="text-primary" size={18} />} href="projects" />
 			</>
 		)}
 	/>,
@@ -49,7 +49,7 @@ export const appNavbarMenuItems = [
 				<AppNavLinkItem title="Loan Requests" icon={<HiBriefcase className="text-primary" size={18} />} href="green-finance/loans" />
 			</>
 		)}
-		show={true}
+		show={false}
 	/>,
 	<AppNavLinkSection
 		title="Company"
