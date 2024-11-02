e7a234c39ae918fcb96948d2cc777e588c87c484
VinGitonga <gitongavincent64@gmail.com>
2024-11-02 06:11:30 +0300

    updated config data

--- src/data/configuration.ts
+++ src/data/configuration.ts
@@ -4,35 +4,35 @@ import { FleetAddVariant, TAddFleetModalData } from "@/types/Fleet";
 export const stationaryCombustionAddEquipmentData = {
 	[StationaryCombustionAddVariant.BOILERS_FURNACES]: {
 		title: "Boilers & Furnaces",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Type of Equipments used by your boilers & furnaces to be used in recording fuel consumption",
+		tooltipText: "Add Boiler/Furnace Equipment",
 	},
 	[StationaryCombustionAddVariant.GENERATORS]: {
 		title: "Generators",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Record the Generators so as to track fuel consumption used by your backup generator.",
+		tooltipText: "Generators",
 	},
 	[StationaryCombustionAddVariant.HEATER]: {
 		title: "Heater",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Record the heating appliances you use",
+		tooltipText: "Heating applicances",
 	},
 	[StationaryCombustionAddVariant.KITCHEN_APPLIANCES]: {
 		title: "Kitchen Appliances",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Record the appliances used in your kitchen.",
+		tooltipText: "Kitchen appliances information",
 	},
 } satisfies TAddEquipmentModalData;
 
 export const fleetsDataInfo = {
 	[FleetAddVariant.DELIVERY]: {
 		title: "Delivery Vehicles",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Record the vehicles used for company deliveries (e.g., company supplies , notebooks ,pens )",
+		tooltipText: "This make it easy for analyzing the emissions of the vehicles used for supplies",
 	},
 	[FleetAddVariant.PASSENGER]: {
 		title: "Passenger Vehicles",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis",
-		tooltipText: "Sed ut perspiciatis unde omnis iste natus error sit",
+		description: "Record the company-owned vehicles used for transporting staff/students e.t.c",
+		tooltipText: "This make it easy for analyzing the emissions of the vehicles used for transportation",
 	},
 } satisfies TAddFleetModalData;
